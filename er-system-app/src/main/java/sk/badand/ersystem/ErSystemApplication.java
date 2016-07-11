4ddff44c02605dce1b5d0882e5101ec2ac649422
Zavael <cccp14@atlas.sk>
2016-07-11 10:28:03 +0200

    Projects showed, plus some security changes

--- er-system-app/src/main/java/sk/badand/ersystem/ErSystemApplication.java
+++ er-system-app/src/main/java/sk/badand/ersystem/ErSystemApplication.java
@@ -3,7 +3,12 @@ package sk.badand.ersystem;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sk.badand.ersystem.domain.EmployeeRepository;
@@ -20,5 +25,21 @@ public class ErSystemApplication {
     @Bean
     CommandLineRunner runner(EmployeeRepository er, ProjectRepository pr) {
         return new CmdLineRunner(er, pr);
+    }
+
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+/*
+            http.httpBasic()
+                    .and().authorizeRequests()
+                    .antMatchers("/index.html", "/home.html", "/login.html", "/")
+                    .permitAll()
+                    .anyRequest().authenticated();
+*/
+        }
     }
 }
